ff348e81dc5525d4d8b79c1469131e1b06a70b52
Aaronsu20170930 <jlsuhome@outlook.com>
2020-04-06 01:32:42 +0800

    first commit

---
+++ sign/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from sign.models import Event
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            request.session['user'] = username
+            response = HttpResponseRedirect('/event_manage/')
+            return response
+        else:
+            return render(request, 'index.html', {'error': '用户名或密码错误'})
+
+
+@login_required
+def event_manage(request):
+    event_list = Event.objects.all()
+    username = request.session.get('user', '')
+    return render(request, 'event_manage.html', {'user': username, 'events': event_list})
