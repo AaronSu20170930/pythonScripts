211bde8e50c7763f588d63877ee3c4e73bdaad1f
AaronSu20170930 <jlsuhome@outlook.com>
2020-04-07 00:02:24 +0800

    views中guest_manage方法

--- sign/views.py
+++ sign/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib import auth
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from sign.models import Event
+from sign.models import Event, Guest
 
 # Create your views here.
 def index(request):
@@ -35,3 +35,10 @@ def search_name(request):
     search_name = request.GET.get('name', '')
     event_list = Event.objects.filter(name__contains=search_name)
     return render(request, 'event_manage.html', {'user': username, 'events': event_list})
+
+
+@login_required
+def guest_manage(request):
+    username = request.session.get('user', '')
+    guest_list = Guest.objects.all()
+    return render(request, "guest_manage.html", {'user': username, 'guests': guest_list})
